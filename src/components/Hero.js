ad62c25e8c1a0ac836abaa16ce52006da6bd9461
ARC Sebastian <arcsbstn@gmail.com>
2021-09-16 19:25:50 +0800

    add useContext(ThemeContext)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../contexts/theme'
+
 
 export default function Hero() {
+  const [{ theme }] = useContext(ThemeContext)
+  const heroStyle = {
+    backgroundColor: theme.heroBg,
+    color: theme.heroText
+  }
+
   return (
-    <section id="hero">
-      <h1> Hi, I'm Adrianne!</h1>
+    <section id="hero"
+      style={heroStyle}>
+      <h1 style={{ color: theme.heroAccent }}> Hi, I'm Adrianne!</h1>
       <h2>Currently seeking opportunities to help build beautiful, useful, and useable experiences.</h2>
     </section>
   )
