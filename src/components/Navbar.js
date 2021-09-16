9f06f19f46c9d6cbc54c0563164f0b8011790020
ARC Sebastian <arcsbstn@gmail.com>
2021-09-16 19:19:34 +0800

    add basic style + toggleTheme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import '../styles/Navbar.scss'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../contexts/theme'
 
-export default function Projects() {
+export default function Navbar() {
+  const [{ theme }, toggleTheme] = useContext(ThemeContext)
+  const stripStyle = {
+    backgroundColor: theme.stripBg,
+    color: theme.stripText
+  }
+
   return (
-    <section id="projects">
-      <div className="class logo">Adrianne Sebastian</div>
-      <ul className="">
+    <section id="navbar"
+      className="Navbar__container"
+      style={stripStyle}>
+      <div className="Navbar__logo">Adrianne Sebastian</div>
+      <ul className="Navbar__links">
         <li>About</li>
         <li>Projects</li>
         <li>Contact</li>
+        <button onClick={toggleTheme}>☺️</button>
       </ul>
     </section>
   )
