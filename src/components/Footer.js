c07b6b11228ea1d7464b355ed3ea344eff731a7f
ARC Sebastian <arcsbstn@gmail.com>
2021-09-17 12:21:16 +0800

    change double quotes to single quotes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <section id="footer">
+    <section id='footer'>
       Adrianne Sebastian 2021
-      <ul className="links">
-        <li><i className="fa fa-github" aria-hidden="true"></i></li>
-        <li><i className="fa fa-codepen" aria-hidden="true"></i></li>
-        <li><i className="fa fa-free-code-camp" aria-hidden="true"></i></li>
-        <li><i className="fa fa-linkedin" aria-hidden="true"></i></li>
-        <li><i className="fa fa-twitter" aria-hidden="true"></i></li>
+      <ul className='links'>
+        <li><i className='fa fa-github' aria-hidden='true'></i></li>
+        <li><i className='fa fa-codepen' aria-hidden='true'></i></li>
+        <li><i className='fa fa-free-code-camp' aria-hidden='true'></i></li>
+        <li><i className='fa fa-linkedin' aria-hidden='true'></i></li>
+        <li><i className='fa fa-twitter' aria-hidden='true'></i></li>
       </ul>
     </section>
   )
