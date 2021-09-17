4910e387deee1ba47bc5904fb7eb3a05f5cc4477
ARC Sebastian <arcsbstn@gmail.com>
2021-09-17 20:18:38 +0800

    add initial markup + styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,34 @@
-import React from "react"
+import '../styles/Projects.scss'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../contexts/theme'
 
 export default function Projects() {
+  const [{ theme }] = useContext(ThemeContext)
+
   return (
-    <section id="projects">
-      <h3>Projects</h3>
+    <section id='projects'
+      style={{ color: theme.projectsAccent }}>
+      <div className="Projects__wrapper">
+        <div className="Projects__primaryBg" style={{ backgroundColor: theme.projectsBg2 }}>
+          <div className="Projects__accentBg row justify-content-center" style={{ backgroundColor: theme.projectsBg1 }}>
+            <h3>Projects</h3>
+          </div>
+          <div className="Projects__contentWrapper container">
+            <div className="row justify-content-center">
+              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
+              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
+            </div>
+            <div className="row justify-content-center">
+              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
+              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
+            </div>
+            <div className="row justify-content-center">
+              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
+              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
