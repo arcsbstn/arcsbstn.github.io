edc55c6e66e5bce22b0e6324204531d66c763d60
ARC Sebastian <arcsbstn@gmail.com>
2021-09-17 09:23:07 +0800

    add theme colors

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -7,6 +7,14 @@ const themes = {
     heroBg: '#292E33',
     heroAccent: '#B16449',
     heroText: '#DAC09E',
+    projectsBg1: '#843226',
+    projectsBg2: '#3A4E52',
+    projectsAccent: '#DFB681',
+    projectsText: '#3A4E52',
+    contactBg: '#292E33',
+    contactAccent: '#B16449',
+    contactTextAreaBg: '#3A4E52',
+    contactTextAreaText: '#D7C1A1'
   },
   light: {
     stripBg: '#9A634F',
@@ -14,6 +22,14 @@ const themes = {
     heroBg: '#FEEEED',
     heroAccent: '#DB846A',
     heroText: '#CAC5B5',
+    projectsBg1: '#CF886F',
+    projectsBg2: '#FADCD1',
+    projectsAccent: '#DFB681',
+    projectsText: '#926553',
+    contactBg: '#B16449',
+    contactAccent: '#CF886F',
+    contactTextAreaBg: '#F9DCD2',
+    contactTextAreaText: '#CF886F'
   }
 }
 
