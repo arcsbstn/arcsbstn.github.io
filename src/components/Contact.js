2c6b303386ff81cc47a51e4b703296e67bc5929b
ARC Sebastian <arcsbstn@gmail.com>
2021-09-17 13:57:47 +0800

    encapsulate form elements in div rows

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,23 +19,30 @@ export default function Contact() {
       <h3 style={{ color: theme.contactAccent }}>Contact</h3>
       <form id='contact'
         name='contact'>
-        <label htmlFor='name'>Name</label>
-        <input id='name'
-          name='name'
-          type='text'
-          style={textareaStyle}
-          placeholder='Your Name'/>
-        <label htmlFor='email'>Email</label>
-        <input id='email'
-          name='email'
-          type='email'
-          style={textareaStyle}
-          placeholder='your@email.com'/>
-        <label htmlFor='message'>Message</label>
-        <textarea id='message'
-          name='message'
-          style={textareaStyle}/>
-        <button type='submit'>Submit</button>
+        <div className="row"><
+          label htmlFor='name'>Name</label>
+          <input id='name'
+            name='name'
+            type='text'
+            style={textareaStyle}
+            placeholder='Your Name' /></div>
+        <div className="row">
+          <label htmlFor='email'>Email</label>
+          <input id='email'
+            name='email'
+            type='email'
+            style={textareaStyle}
+            placeholder='your@email.com' />
+        </div>
+        <div className="row">
+          <label htmlFor='message'>Message</label>
+          <textarea id='message'
+            name='message'
+            style={textareaStyle} />
+        </div>
+        <div className="row">
+          <button type='submit'>Submit</button>
+        </div>
       </form>
     </section>
   )
