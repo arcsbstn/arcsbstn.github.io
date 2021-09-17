e74372290957f10ab494d17358c6d622467f38d8
ARC Sebastian <arcsbstn@gmail.com>
2021-09-17 13:19:56 +0800

    simultaneously toggle icon with theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,12 @@ import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/theme'
 
 export default function Navbar() {
-  const [{ theme }, toggleTheme] = useContext(ThemeContext)
+  const [{ theme, isDark }, toggleTheme] = useContext(ThemeContext)
   const stripStyle = {
     backgroundColor: theme.stripBg,
     color: theme.stripText
   }
+  let toggleIcon = isDark ? 'fa fa-sun-o' : 'fa fa-moon-o'
 
   return (
     <section id="navbar"
@@ -18,7 +19,9 @@ export default function Navbar() {
         <li>About</li>
         <li>Projects</li>
         <li>Contact</li>
-        <button onClick={toggleTheme}>☺️</button>
+        <button onClick={toggleTheme}>
+          <i className={toggleIcon} aria-hidden='true' />
+        </button>
       </ul>
     </section>
   )
