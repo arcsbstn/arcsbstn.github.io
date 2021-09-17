726baa210d90ec371a323849ffa1a46952789162
ARC Sebastian <arcsbstn@gmail.com>
2021-09-17 09:24:26 +0800

    add codepen icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,9 @@ export default function Footer() {
       Adrianne Sebastian 2021
       <ul className="links">
         <li><i className="fa fa-github" aria-hidden="true"></i></li>
-        <li><i className="fa fa-linkedin" aria-hidden="true"></i></li>
+        <li><i className="fa fa-codepen" aria-hidden="true"></i></li>
         <li><i className="fa fa-free-code-camp" aria-hidden="true"></i></li>
+        <li><i className="fa fa-linkedin" aria-hidden="true"></i></li>
         <li><i className="fa fa-twitter" aria-hidden="true"></i></li>
       </ul>
     </section>
