1e4f4e92dab25423ce9a63cdbe1bfcd8afe4f211
ARC Sebastian <arcsbstn@gmail.com>
2021-09-17 13:18:50 +0800

    fix footer blurb + add color styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import '../styles/Footer.scss'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../contexts/theme'
 
 export default function Footer() {
+  const [{theme}] = useContext(ThemeContext)
+  const footerStyle = {
+    backgroundColor: theme.stripBg,
+    color: theme.stripText
+  }
+
   return (
-    <section id='footer'>
-      Adrianne Sebastian 2021
-      <ul className='links'>
+    <section id='footer'
+      style={footerStyle}>
+      Made with <i className='fa fa-heart' aria-hidden='true'></i> | Adrianne Sebastian 2021
+      <ul className='Footer__links'>
         <li><i className='fa fa-github' aria-hidden='true'></i></li>
         <li><i className='fa fa-codepen' aria-hidden='true'></i></li>
         <li><i className='fa fa-free-code-camp' aria-hidden='true'></i></li>
