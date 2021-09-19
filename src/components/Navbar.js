44109718583389d2c2d144e9d534a1ac7464774f
ARC Sebastian <arcsbstn@gmail.com>
2021-09-19 15:00:48 +0800

    add toggle icon style, Navbar_wrapper

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,21 +10,31 @@ export default function Navbar() {
     color: theme.stripText
   }
   let toggleIcon = isDark ? 'fa fa-sun-o' : 'fa fa-moon-o'
+  const toggleIconStyle = {
+    color: theme.stripText,
+    border: `1px solid ${theme.stripText}`,
+    borderRadius: '100%',
+    paddingTop: '5px',
+    width: '30px',
+    height: '30px'
+  }
 
   return (
-    <section id="navbar"
-      className="Navbar__container"
+    <section id='navbar'
+      className='Navbar__container'
       style={stripStyle}>
-      <div className="General__wrapper">
-        <div className="Navbar__logo">Adrianne Sebastian</div>
-        <ul className="Navbar__links">
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact</li>
-          <button onClick={toggleTheme}>
-            <i className={toggleIcon} aria-hidden='true' />
-          </button>
-        </ul>
+      <div className='General__wrapper'>
+        <div className='Navbar__wrapper'>
+          <div className='Navbar__logo'>Adrianne Sebastian</div>
+          <ul className='Navbar__links'>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+            <button onClick={toggleTheme}>
+              <i className={toggleIcon} aria-hidden='true' style={toggleIconStyle} />
+            </button>
+          </ul>
+        </div>
       </div>
     </section>
   )
