8afd128a5c051de84226c7b0be5f3e0574eafdc9
ARC Sebastian <arcsbstn@gmail.com>
2021-09-22 14:45:05 +0800

    update resume file + add rel='noreferrer' in link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import Document from '../arcsbstn-resume-2021b.pdf'
+import Document from '../assets/arcsbstn-2021.pdf'
 import { ThemeContext } from '../contexts/theme'
 
 export default function Resume() {
@@ -7,7 +7,7 @@ export default function Resume() {
 
   return (
     <span className='resume'>
-      <a href={Document} target='_blank' style={{color: theme.heroAccent}}><i class="fa fa-user" /> Resume</a>
+      <a href={Document} target='_blank' rel='noreferrer' style={{color: theme.heroAccent}}><i className='fa fa-user' /> Resume</a>
     </span>
   )
 }
