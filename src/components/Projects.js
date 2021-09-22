aadf372f2b6611bdebd480ea088be90c446552da
ARC Sebastian <arcsbstn@gmail.com>
2021-09-22 16:09:40 +0800

    render projects from projectsList

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import '../styles/Projects.scss'
 import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/theme'
+import { projects } from '../projectsList'
 
 export default function Projects() {
   const [{ theme }] = useContext(ThemeContext)
@@ -8,23 +9,25 @@ export default function Projects() {
   return (
     <section id='projects'
       style={{ color: theme.projectsAccent }}>
-      <div className="Projects__wrapper">
-        <div className="Projects__primaryBg" style={{ backgroundColor: theme.projectsBg2 }}>
-          <div className="Projects__accentBg row justify-content-center" style={{ backgroundColor: theme.projectsBg1 }}>
+      <div className='Projects__wrapper'>
+        <div className='Projects__primaryBg' style={{ backgroundColor: theme.projectsBg2 }}>
+          <div className='Projects__accentBg row justify-content-center' style={{ backgroundColor: theme.projectsBg1 }}>
             <h3>Projects</h3>
           </div>
-          <div className="Projects__contentWrapper container">
-            <div className="row justify-content-center">
-              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
-              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
-            </div>
-            <div className="row justify-content-center">
-              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
-              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
-            </div>
-            <div className="row justify-content-center">
-              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
-              <div className="Projects__tile col-md-4" style={{ backgroundColor: theme.projectsAccent }}></div>
+          <div className='General__wrapper'>
+            <div className='Projects__contentWrapper container d-flex flex-wrap flex-row'>
+              <div className='row w-100 justify-content-center'>
+                {projects.map((project, index) => {
+                  return (
+                    <div key={index}
+                      className='Projects__tile col-md-5'
+                      style={{
+                        backgroundImage: `url(${project.image})`
+                      }}>
+                    </div>
+                  )
+                })}
+              </div>
             </div>
           </div>
         </div>
