c2e297e8316a33dd08a1c4058bedd59d6a8a95a0
ARC Sebastian <arcsbstn@gmail.com>
2021-09-23 14:01:51 +0800

    update footer text, add links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,14 +14,13 @@ export default function Footer() {
     <section id='footer'
       style={footerStyle}>
       <div className="General__wrapper">
-        <p>Adrianne Sebastian 2021</p>
-        <p>Made with <i className='fa fa-heart' aria-hidden='true'></i></p>
+        <p>Made with <i className='fa fa-heart' aria-hidden='true'></i> by Adrianne Sebastian</p>
         <ul className='Footer__links'>
-          <li><i className='fa fa-github' aria-hidden='true'></i></li>
-          <li><i className='fa fa-codepen' aria-hidden='true'></i></li>
-          <li><i className='fa fa-free-code-camp' aria-hidden='true'></i></li>
-          <li><i className='fa fa-linkedin' aria-hidden='true'></i></li>
-          <li><i className='fa fa-twitter' aria-hidden='true'></i></li>
+          <li><a href='https://github.com/arcsbstn/' target='_blank' style={{ color: theme.stripText }}><i className='fa fa-github' aria-hidden='true'></i></a></li>
+          <li><a href='https://codepen.io/arcsbstn/' target='_blank' style={{ color: theme.stripText }}><i className='fa fa-codepen' aria-hidden='true'></i></a></li>
+          <li><a href='https://freecodecamp.org/arcsbstn' target='_blank' style={{ color: theme.stripText }}><i className='fa fa-free-code-camp' aria-hidden='true'></i></a></li>
+          <li><a href='https://linkedin.com/in/arcsbstn' target='_blank' style={{ color: theme.stripText }}><i className='fa fa-linkedin' aria-hidden='true'></i></a></li>
+          <li><a href='https://twitter.com/arcsbstn' target='_blank' style={{ color: theme.stripText }}><i className='fa fa-twitter' aria-hidden='true'></i></a></li>
         </ul>
       </div>
     </section>
