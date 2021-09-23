db42d0f6f30b5024c4f952bf66978ac143b733d8
ARC Sebastian <arcsbstn@gmail.com>
2021-09-23 13:21:58 +0800

    add Contact__contentWrapper, update button colors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import React, { useContext, useState } from 'react'
 import { ThemeContext } from '../contexts/theme'
 
 export default function Contact() {
-  const [{ theme }] = useContext(ThemeContext)
+  const [{ theme, isDark }] = useContext(ThemeContext)
   const contactStyle = {
     backgroundColor: theme.contactBg,
     color: theme.contactTextareaText
@@ -12,6 +12,10 @@ export default function Contact() {
   const textareaStyle = {
     backgroundColor: theme.contactTextareaBg,
     color: theme.contactTextareaText
+  }
+  const buttonStyle = {
+    backgroundColor: theme.contactAccent,
+    color: isDark ? theme.contactTextareaText : theme.contactTextareaBg
   }
 
   const [name, setName] = useState('')
@@ -38,45 +42,47 @@ export default function Contact() {
   return (
     <section id='contact'
       style={contactStyle}>
-      <div className="General__wrapper">
-        <h3 style={{ color: theme.contactAccent }}>Contact</h3>
-        <form id='contact'
-          name='contact'
-          onSubmit={handleSubmit}
-          method='POST'
-          data-netlify='true'>
-          <div className='row'>
-            <label htmlFor='name'>Name</label>
-            <input id='name'
-              name='name'
-              type='text'
-              onChange={(e) => { setName(e.target.value) }}
-              style={textareaStyle}
-              placeholder='Your Name' />
-          </div>
-          <div className='row'>
-            <label htmlFor='email'>Email</label>
-            <input id='email'
-              name='email'
-              type='email'
-              onChange={(e) => { setEmail(e.target.value) }}
-              style={textareaStyle}
-              placeholder='your@email.com' />
-          </div>
-          <div className='row'>
-            <label htmlFor='message'>Message</label>
-            <textarea id='message'
-              name='message'
-              onChange={(e) => { setMessage(e.target.value) }}
-              style={textareaStyle} />
-          </div>
-          <div className="row">
-            <div data-netlify-recaptcha='true'></div>
-          </div>
-          <div className='row'>
-            <button type='submit'>Submit</button>
-          </div>
-        </form>
+      <div className='General__wrapper'>
+        <div className='Contact__contentWrapper'>
+          <h3 style={{ color: theme.contactAccent }}>Contact</h3>
+          <form id='contact'
+            name='contact'
+            onSubmit={handleSubmit}
+            method='POST'
+            data-netlify='true'>
+            <div className='row'>
+              <label htmlFor='name'>Name</label>
+              <input id='name'
+                name='name'
+                type='text'
+                onChange={(e) => { setName(e.target.value) }}
+                style={textareaStyle}
+                placeholder='Your Name' />
+            </div>
+            <div className='row'>
+              <label htmlFor='email'>Email</label>
+              <input id='email'
+                name='email'
+                type='email'
+                onChange={(e) => { setEmail(e.target.value) }}
+                style={textareaStyle}
+                placeholder='your@email.com' />
+            </div>
+            <div className='row'>
+              <label htmlFor='message'>Message</label>
+              <textarea id='message'
+                name='message'
+                onChange={(e) => { setMessage(e.target.value) }}
+                style={textareaStyle} />
+            </div>
+            <div className='row'>
+              <div data-netlify-recaptcha='true'></div>
+            </div>
+            <div className='row'>
+              <button type='submit' style={buttonStyle}>Submit</button>
+            </div>
+          </form>
+        </div>
       </div>
     </section>
   )
