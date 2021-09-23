89a38368d7858c62958d4108fa51c433523dedb8
ARC Sebastian <arcsbstn@gmail.com>
2021-09-23 19:59:04 +0800

    add burger menu markup

--- src/components/nav/MobNav.js
+++ src/components/nav/MobNav.js
@@ -1,10 +1,15 @@
 import '../../styles/Navbar.scss'
 import NavLinks from './NavLinks'
+import { useState } from 'react'
 
 export default function MobNav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
     <nav className='Navbar__mobNav'>
-      <NavLinks />
+      <i className='fa fa-bars hamburger' onClick={() => setIsMenuOpen(!isMenuOpen)}></i>
+      <div className='Navbar__mobNavLinks'>
+        {isMenuOpen && <NavLinks />}
+      </div>
     </nav>
   )
 }
