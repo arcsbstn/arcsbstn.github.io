32e9879c88548ecdda82f21ef6acb7cb93cdc6c3
ARC Sebastian <arcsbstn@gmail.com>
2021-09-24 16:08:55 +0800

    update 2-col -> 1-col layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,15 +15,16 @@ export default function Projects() {
           </div>
           <div className='General__wrapper'>
             <div className='Projects__contentWrapper container d-flex flex-wrap flex-row'>
-          <h3>Projects</h3>
-
-              <div className='row w-100 justify-content-center'>
-                {projects.map((project, index) => {
-                  return (
+              <h3>Projects</h3>
+              {projects.map((project, index) => {
+                return (
+                  <div className="row w-100 justify-content-center">
                     <div key={index}
-                      className='Projects__tile col-md-5'
+                      className='Projects__tile col-md-6'
                       style={{ backgroundImage: `url(${project.image})` }}>
-                      <div className="Projects__info Projects__info--blur">
+                    </div>
+                    <div className="Projects__infoWrapper col-md-4">
+                      <div className="Projects__info">
                         <h4>{project.title}</h4>
                         <h5>{project.subtitle}</h5>
                         <p>{project.description}</p>
@@ -31,9 +32,9 @@ export default function Projects() {
                         <a href={project.repoLink}>Source Code</a>
                       </div>
                     </div>
-                  )
-                })}
-              </div>
+                  </div>
+                )
+              })}
             </div>
           </div>
         </div>
