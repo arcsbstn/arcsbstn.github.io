bafccc1bef303de413232a4688bba1d0e258a1e0
ARC Sebastian <arcsbstn@gmail.com>
2023-07-04 15:50:32 +0800

    add basic experience list

--- src/components/content/Experience.tsx
+++ src/components/content/Experience.tsx
@@ -1,11 +1,48 @@
 export default function Experience() {
-  const experienceList = ["exp 1", "exp 2", "exp 3"];
+  const experienceList = [
+    {
+      role: "Software Developer",
+      organization: "Full Potential Solutions, Inc.",
+      startEndDates: "2021 Oct - Present",
+      description: "",
+      techStack: [
+        "JavaScript",
+        "Twilio",
+        "ReactJS",
+        "Node.js",
+        "Redux",
+        "Postman",
+      ],
+    },
+    {
+      role: "Junior Software Engineer",
+      organization: "Reekoh, Inc.",
+      startEndDates: "2019 May - 2021 Apr",
+      description: "",
+      techStack: ["JavaScript", "Node.js", "Postman"],
+    },
+    {
+      role: "Associate Software Engineer",
+      organization: "Accenture, Inc.",
+      startEndDates: "2018 Feb - 2019 Apr",
+      description: "",
+      techStack: [],
+    },
+  ];
   return (
     <div>
       <h3>Experience</h3>
-      {experienceList.map((experience) => (
-        <div>{experience}</div>
-      ))}
+      {experienceList.map(
+        ({ organization, role, startEndDates, description, techStack }) => (
+          <div>
+            <h4>{role}</h4>
+            <h5>{organization}</h5>
+            <h6>{startEndDates}</h6>
+            <p>{description}</p>
+            <p>Technologies: {techStack.join(", ")}</p>
+          </div>
+        )
+      )}
     </div>
   );
 }
