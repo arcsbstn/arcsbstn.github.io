fe2b6e5b7d140844aec254c0926ea43ae36c0446
ARC Sebastian <arcsbstn@gmail.com>
2023-08-08 21:33:28 +0800

    fix font sizing

--- src/components/sidebar/Hero.tsx
+++ src/components/sidebar/Hero.tsx
@@ -4,9 +4,13 @@ export default function Hero() {
       <h1>
         <a href="/">Adrianne Sebastian</a>
       </h1>
-      <h2>
+      <h2 style={{ fontSize: "1.3em" }}>
         Software Developer at{" "}
-        <a target="_blank" rel="noreferrer" href="https://www.fpsinc.com/aboutus">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.fpsinc.com/aboutus"
+        >
           Full Potential Solutions
         </a>
       </h2>
