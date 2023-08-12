362c84ebea303fafcd230cfa054c658706471d91
ARC Sebastian <arcsbstn@gmail.com>
2023-08-12 21:20:18 +0800

    add animation to nav

--- src/components/sidebar/Nav.tsx
+++ src/components/sidebar/Nav.tsx
@@ -5,8 +5,13 @@ import { content } from "../../constants";
 import { ActiveSectionContext } from "../../ResumePortfolio";
 
 interface IHandleClick {
-  e: React.MouseEvent<HTMLUListElement>;
+  e: React.MouseEvent<HTMLSpanElement>;
   section: string;
+}
+
+interface INavLi {
+  activeSection: string;
+  currentSection: string;
 }
 
 function handleClick({ e, section }: IHandleClick) {
@@ -14,30 +19,57 @@ function handleClick({ e, section }: IHandleClick) {
   window.location.replace(`/#${section}`);
 }
 
-const StyledLi = styled.li<{ $isActiveSection?: boolean }>`
+const AnimatedDash = styled.div<{ $isActiveSection?: boolean }>`
+  position: relative;
+  top: -4px;
+  margin-right: 24px;
+  height: 1.5px;
+  width: ${(props) => (props.$isActiveSection ? "60px" : "20px")};
+  background-color: ${(props) => (props.$isActiveSection ? "blue" : "black")};
+  display: inline-block;
+  -webkit-transition: -webkit-transform 0.3s, width 0.5s;
+  -moz-transition: -webkit-transform 0.3s, width 0.5s;
+  transition: transform 0.3s, width 0.5s;
+`;
+
+const StyledSpan = styled.span<{ $isActiveSection?: boolean }>`
+  font-size: 0.9em;
+
   cursor: pointer;
   color: ${(props) => {
     return props.$isActiveSection ? "blue" : "black";
   }};
 `;
 
+const NavUl = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+`;
+
+const NavLi = ({ activeSection, currentSection }: INavLi) => (
+  <li>
+    <AnimatedDash $isActiveSection={activeSection === currentSection} />
+    <StyledSpan
+      $isActiveSection={activeSection === currentSection}
+      onClick={(e: React.MouseEvent<HTMLSpanElement>) =>
+        handleClick({ e, section: currentSection })
+      }
+      children={currentSection.toUpperCase()}
+    />
+  </li>
+);
+
 export default function Nav() {
   const [activeSection] = useContext(ActiveSectionContext);
 
   return (
-    <ul>
-      <StyledLi
-        $isActiveSection={activeSection === content.EXPERIENCE}
-        onClick={(e: any) => handleClick({ e, section: content.EXPERIENCE })}
-      >
-        Experience
-      </StyledLi>
-      <StyledLi
-        $isActiveSection={activeSection === content.PROJECTS}
-        onClick={(e: any) => handleClick({ e, section: content.PROJECTS })}
-      >
-        Projects
-      </StyledLi>
-    </ul>
+    <NavUl>
+      <NavLi activeSection={activeSection} currentSection={content.EXPERIENCE} />
+      <NavLi activeSection={activeSection} currentSection={content.PROJECTS} />
+    </NavUl>
   );
 }
