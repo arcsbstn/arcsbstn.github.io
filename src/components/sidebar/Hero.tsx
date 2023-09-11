06cb4680e858c77c2caf2c0f0b2c2d6ec0a5a890
ARC Sebastian <arcsbstn@gmail.com>
2023-09-11 16:30:03 +0800

    link text decoration quick fix

--- src/components/sidebar/Hero.tsx
+++ src/components/sidebar/Hero.tsx
@@ -2,17 +2,10 @@ export default function Hero() {
   return (
     <div>
       <h1>
-        <a href="/">Adrianne Sebastian</a>
+        <a href="/" style={{textDecoration: 'none', fontSize: "0.8em"}}>Adrianne Sebastian</a>
       </h1>
-      <h2 style={{ fontSize: "1.3em" }}>
-        Software Developer at{" "}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.fpsinc.com/aboutus"
-        >
-          Full Potential Solutions
-        </a>
+      <h2 style={{ fontSize: "1.2em" }}>
+        Software Developer
       </h2>
       <p>
         Crafting experiences for the web. I help organizations create products
